fad6d07699c06f3ce40072ef48623148874492ed
mi nombre <andre.abregum@gmail.com>
2022-02-16 13:38:48 -0500

    Andre-correcion_Index

---
+++ criptonyta/components/Header.component.js
@@ -0,0 +1,14 @@
+const Header = () => {
+    return <div>
+        <div className="row">
+            <div className="col-md-8">
+
+            </div>
+            <div className="col-6 col-md-4">
+                <a href="#">Abrir Cuenta</a>
+                <a className="btn btn-success" href="#">Iniciar Sesión</a>
+            </div>
+        </div>
+    </div>
+}
+export default Header
--- criptonyta/pages/index.js
+++ criptonyta/pages/index.js
@@ -1,25 +1,30 @@
 import Banner from "../components/Banner.Components"
+import Footer from "../components/Footer.component"
+import Header from "../components/Header.component"
 import Login from "../components/Login.Components"
 import PiePagina from "../components/PiePagina.Components"
 export default function Home() {
   return <div >
     <div >
-        <h1>Grupo 5</h1>
-        <div className="d-grid gap-2 d-md-flex justify-content-md-end btn-4" >
-        <Login/>
+      <Header />
+      <h1>Grupo 5</h1>
+      <div className="row">
+        <div className="col">
+          <Banner />
         </div>
-        <div className="btn-4">
-        <Banner/>
-
+        <div className="col">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipiscing elit, ultricies penatibus dui sociis a mollis,
+            vivamus aenean euismod dignissim conubia porttitor. Montes lobortis ante lectus hendrerit erat metus
+            eu nisi in enim non sociosqu, odio luctus sagittis turpis donec eget hac bibendum nisl commodo. Malesuada
+            porttitor quisque euismod facilisi eros pellentesque convallis cursus interdum, laoreet lectus nascetur integer
+            libero at arcu accumsan sociis viverra, vehicula justo primis netus montes rutrum tellus penatibus.
+          </p>
         </div>
-        
-        
-        
-
+      </div>
+      <div>
+        <Footer />
+      </div>
     </div>
-    <div>
-      <PiePagina/>
-    </div>
-    </div>
-
+  </div>
 }
