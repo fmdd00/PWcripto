299225151bb81ac617a95ac6e56bafead1ac018a
Saori Arana <20180105@aloe.ulima.edu.pe>
2022-02-17 14:19:51 -0500

    avance

--- criptonyta/pages/EnviarVender.js
+++ criptonyta/pages/EnviarVender.js
@@ -1,8 +1,10 @@
 import VentaCabecera from "../components/VentaCabecera.Components"
 import Footer4 from "../components/Footer4.Components"
+import CuerpoVenta from "../components/CuerpoVenta.Components"
 export default function VerTipoCambio() {
     return <div>
         <VentaCabecera />
+        <CuerpoVenta/>
         
 
        
