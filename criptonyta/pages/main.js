b3fd7b5806928b9f8c8ec9252b2e3766b72fa52d
fmdd00 <francisco63554@hotmail.com>
2022-02-17 00:11:47 -0500

    actualizada el mainpage

--- criptonyta/pages/main.js
+++ criptonyta/pages/main.js
@@ -1,8 +1,12 @@
 import Encabezado from "../components/Encabezado.Components"
+import Footer2 from "../components/Footer2.Components"
+import Bienvenida from "../components/Bienvenida.component"
 
 export default function MainPage() {
     return <div>
         <h1>Crypto-Nita</h1>
         <Encabezado />
+        <Bienvenida />
+        <Footer2 />
     </div>
 }
