c371461497f47d59a1cfc87229afa47ba9f52423
fmdd00 <francisco63554@hotmail.com>
2022-02-17 00:49:34 -0500

    el registro ahora es por pasos

---
+++ criptonyta/components/RegistroPaso2.component.js
@@ -0,0 +1,41 @@
+import { useRouter } from 'next/router'
+
+export default function formatoRegistroPaso2() {
+
+    const router = useRouter()
+
+    return <aside >
+        <div className="card">
+            <div className="card-body">
+                <h3 className="text-center">Paso 2 de 2</h3>
+                <form>
+                    <div className="mt-4 input-group">
+                        <input id="txt_email" type="text" className="form-control" placeholder="Dirección de correo" />
+                    </div>
+                    <div className="mt-2">
+                        <input id="txt_password" type="password" className="form-control" placeholder="Contraseña" />
+                    </div>
+                    <div className="mt-2">
+                        <input id="txt_password" type="password" className="form-control" placeholder="Repite la contraseña" />
+                    </div>
+                    <div className="mt-2 input-group">
+                        <span class="input-group-text" id="cod_pe">+ 51</span>
+                        <input id="txt_username" type="number" className="form-control" placeholder="Número telefónico" />
+                    </div>
+                    <div class="form-check mt-4">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                            <label class="form-check-label" for="flexCheckDefault">
+                                <p> Acepto los <a className="link-primary" href="#">Términos y Condiciones</a> de Crypto-Nita 
+                                    y la <a className="link-primary" href="#">Política de privacidad</a> y autorizo el tratamiento 
+                                    de mis datos personales de conformidad con la Política de Privacidad.</p>
+                            </label>
+                    </div>
+                    <div className="d-grid gap-2 mt-2">
+                        <a id="butLogin" className="btn btn-primary" type="button" href="/main">Finalizar</a>
+                        <button id="butLogin" className="btn btn-secondary" type="button" onClick={ () => router.back() }>Regresar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </aside>
+}
--- criptonyta/components/RegistroPrincipal.component.js
+++ criptonyta/components/RegistroPrincipal.component.js
@@ -1,31 +1,28 @@
+import { useRouter } from 'next/router'
+
+
 export default function formatoRegistro() {
+
+    const router = useRouter()
+
     return <aside >
         <div className="card">
             <div className="card-body">
-                <h3 className="text-center">Regístrate</h3>
+                <h3 className="text-center">Paso 1 de 2</h3>
                 <form>
                     <div className="mt-4 input-group">
-                        <input id="txt_email" type="text" className="form-control" placeholder="Tu dirección de correo" />
+                        <input id="txt_email" type="text" className="form-control" placeholder="Nombres" />
                     </div>
                     <div className="mt-2 input-group">
-                        <input id="txt_username" type="text" className="form-control" placeholder="Usuario" />
+                        <input id="txt_username" type="text" className="form-control" placeholder="Apellidos" />
                     </div>
                     <div className="mt-2">
-                        <input id="txt_password" type="password" className="form-control" placeholder="Contraseña" />
+                        <input id="txt_password" type="number" className="form-control" placeholder="Número de DNI" />
                     </div>
-                    <div className="mt-2">
-                        <input id="txt_password" type="password" className="form-control" placeholder="Repite tu contraseña" />
-                    </div>
-                    <div class="form-check mt-4">
-                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
-                            <label class="form-check-label" for="flexCheckDefault">
-                                <p> Acepto los <a className="link-primary" href="#">Términos y Condiciones</a> de Crypto-Nita 
-                                    y la <a className="link-primary" href="#">Política de privacidad</a> y autorizo el tratamiento 
-                                    de mis datos personales de conformidad con la Política de Privacidad.</p>
-                            </label>
-                    </div>
+                    
                     <div className="d-grid gap-2 mt-2">
-                        <button id="butLogin" className="btn btn-primary" type="button">Crear cuenta</button>
+                        <a id="butLogin" className="btn btn-primary" type="button" href="/Registro2">Siguiente</a>
+                        <button id="butLogin" className="btn btn-secondary" type="button" onClick={ () => router.back() }>Regresar</button>
                     </div>
                 </form>
             </div>
