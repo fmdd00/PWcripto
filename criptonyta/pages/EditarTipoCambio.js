7782948eea77256c894e1ac3314e3f173293d485
Saori Arana <20180105@aloe.ulima.edu.pe>
2022-02-17 14:54:01 -0500

    Historia 20-Saori

--- criptonyta/pages/EditarTipoCambio.js
+++ criptonyta/pages/EditarTipoCambio.js
@@ -1,14 +1,29 @@
 import VentaCabecera from "../components/VentaCabecera.Components";
 import Paginacion from "../components/Paginacion.Components"
+import Footer2 from "../components/Footer2.Components"
 export default function EditarTipoCambio() {
     return <div >
         <VentaCabecera/>
         
         <Paginacion/>
-        <h2 className="Edit mt-4">Editar Tipo de Cambio</h2>
-        <div className="">
-
+        
+        <div className="col-6">
+        <div className="col">
+        <h2 >BTC-PEN</h2>
+        <div className="col-auto">
+        <input className="form-control form-control-lg" type="text" placeholder="X X 1" aria-label=".form-control-lg example"/>
+  </div>
+        </div>
+        
+        <div className="col">
+        <h2 >PEN-BTC</h2>
+        <input className="form-control form-control-lg" type="text" placeholder="X X 2" aria-label=".form-control-lg example"/>
+        </div>
+        
+            </div>
+            <button className="btn btn-primary btn-5" type="submit">Cambiar</button>
+      
+       <Footer2/>
         </div>
 
-    </div>
 }
