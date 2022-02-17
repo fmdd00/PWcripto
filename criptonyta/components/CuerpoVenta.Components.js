639bcf6762d18420198a29ae12ce11e8ce4ac9d4
Saori Arana <20180105@aloe.ulima.edu.pe>
2022-02-17 13:35:34 -0500

    Venta-Saori

---
+++ criptonyta/components/CuerpoVenta.Components.js
@@ -0,0 +1,16 @@
+const CuerpoVenta=()=>{
+    return <div>
+
+        <h3 className="mt-5">Número de Operación:</h3>
+        <p className="mt-2">00000000111111222222222222</p>
+
+        <input class="form-control form-control-lg mt-5" type="text" placeholder="Número de Transacción" aria-label=".form-control-lg example"/>
+        <div class="d-grid gap-2 col-6 mx-auto">
+  <button class="btn btn-primary mt-5" type="button">Enviar</button>
+ 
+</div>
+
+    </div>
+}
+
+export default CuerpoVenta
---
+++ criptonyta/pages/Venta.js
@@ -0,0 +1,15 @@
+import VentaCabecera from "../components/VentaCabecera.Components";
+import Footer4 from "../components/Footer4.Components";
+import CuerpoVenta from "../components/CuerpoVenta.Components";
+
+export default function Venta() {
+    return <div >
+        <VentaCabecera/>
+        <CuerpoVenta/>
+
+
+
+        <Footer4/>
+
+    </div>
+}
