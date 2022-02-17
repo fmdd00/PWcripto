23b6780eca60f8d263abc546f27d7b262c04f614
Saori Arana <20180105@aloe.ulima.edu.pe>
2022-02-16 22:54:56 -0500

    avance-Saori

--- criptonyta/pages/TerminosCondiciones.js
+++ criptonyta/pages/TerminosCondiciones.js
@@ -1,20 +1,28 @@
 import Regresar from "../components/Regresar.Components"
 export default function TerminosCondiciones() {
 
-    return <div >
+    return <div className="" >
 
         <div className="row mt-4">
-            <div className="col">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfyzUJU59lCBhSjyrj7oIDypW9tckwRR_VuA&usqp=CAU" />
+            <div className="col mt-4 ms-4 ">
+                <img className="rounded mx-auto d-block" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfyzUJU59lCBhSjyrj7oIDypW9tckwRR_VuA&usqp=CAU" />
             </div>
             <div className="col mt-4">
                 <h2>Términos y Condiciones</h2>
                 <p>
-                    Lorem ipsum dolor sit amet consectetur adipiscing elit, ultricies penatibus dui sociis a mollis,
-                    vivamus aenean euismod dignissim conubia porttitor. Montes lobortis ante lectus hendrerit erat metus
-                    eu nisi in enim non sociosqu, odio luctus sagittis turpis donec eget hac bibendum nisl commodo. Malesuada
-                    porttitor quisque euismod facilisi eros pellentesque convallis cursus interdum, laoreet lectus nascetur integer
-                    libero at arcu accumsan sociis viverra, vehicula justo primis netus montes rutrum tellus penatibus.
+                    1.-Al acceder a este sitio web, asumimos que aceptas estos términos y condiciones en su totalidad.
+                    2.-No continues usando el sitio web Crypto-Nyta si no aceptas todos los términos y condiciones establecidos
+                    en esta página.
+                    La siguiente terminología se aplica a estos Términos y Condiciones.
+                    3.-Declaración de Privacidad y Aviso legal y cualquiera o todos los acuerdos:
+                      3.1.-El Cliente, Usted y Su se refiere a usted, la persona que 
+                      accede a este sitio web y acepta los términos y condiciones de la Compañía.
+                      3.2.-La Compañía,Nosotros mismos, Nosotros y Nuestro, se refiere a nuestra 
+                      Compañía.
+                      3.3.-Parte, Partes o Nosotros, se refiere en conjunto al Cliente y a nosotros
+                      mismos, o al Cliente o a nosotros mismos. Todos los términos se refieren a la
+                      oferta.
+                    
                 </p>
             </div>
         </div>
