3404fbae68cb65c1f9c5ec80e00934a28de4376c
Saori Arana <20180105@aloe.ulima.edu.pe>
2022-02-17 14:44:11 -0500

    Avance Editar

---
+++ criptonyta/components/Paginacion.Components.js
@@ -0,0 +1,14 @@
+const Paginacion=()=>{
+    return <nav aria-label="...">
+    <ul className="pagination justify-content-center mt-4">
+    <li className="page-item"><a className="page-link" href="#">Clientes</a></li>
+      <li className="page-item active" aria-current="page">
+        <span className="page-link">Tipo de Cambio</span>
+      </li>
+      
+      <li className="page-item"><a className="page-link" href="#">Operaciones</a></li>
+    </ul>
+  </nav>
+}
+
+export default Paginacion
---
+++ criptonyta/pages/EditarTipoCambio.js
@@ -0,0 +1,14 @@
+import VentaCabecera from "../components/VentaCabecera.Components";
+import Paginacion from "../components/Paginacion.Components"
+export default function EditarTipoCambio() {
+    return <div >
+        <VentaCabecera/>
+        
+        <Paginacion/>
+        <h2 className="Edit mt-4">Editar Tipo de Cambio</h2>
+        <div className="">
+
+        </div>
+
+    </div>
+}
