069366b2a72974a911c10187d27e1c60a0ba70f2
Saori Arana <20180105@aloe.ulima.edu.pe>
2022-02-17 13:07:42 -0500

    AvanceDivisa

---
+++ criptonyta/pages/CambioDivisa.js
@@ -0,0 +1,18 @@
+import Footer2 from "../components/Footer2.Components";
+import CuerpoCambioDivisa from "../components/CuerpoCambioDivisa.Components";
+export default function Home() {
+    return <div>
+
+        <h1 className="mt-4">Cambio Divisa</h1>
+
+
+        <CuerpoCambioDivisa/>
+        <Footer2/>
+
+
+
+
+
+    </div>
+
+}
