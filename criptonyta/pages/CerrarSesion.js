d130c4e03dee8a297208ae4a82a8ddf7b5ed0fa8
Saori Arana <20180105@aloe.ulima.edu.pe>
2022-02-28 22:00:57 -0500

    Cerrar sesion corregido-historia21

--- criptonyta/pages/CerrarSesion.js
+++ criptonyta/pages/CerrarSesion.js
@@ -1,15 +1,15 @@
-import Activa from "../components/Activa.Components"
-import EncabezadoOpciones from "../components/EncabezadoOpciones.Components"
+
+import NavegacionAdmin from "../components/NavAdmin.Component"
 import OpcionesCerrarSesion from "../components/OpcionesCerrarSesion.Components"
 import Footer2 from "../components/Footer2.Components"
 export default function CerrarSesion() {
     return <div>
-        <Activa/>
-        <EncabezadoOpciones />
+         
+        <NavegacionAdmin />
         <div className="row ">
             <div className="col-3 mt-4"><OpcionesCerrarSesion /></div>
-            <div className="col-3"><img className="ratio ratio-10x9 " src="https://static.vecteezy.com/system/resources/previews/003/533/616/non_2x/user-icon-in-flat-style-person-icon-user-icon-for-web-site-user-icon-illustration-vector.jpg" />
-                <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Nombre de Usuario" />
+            <div className="col-3"><img className="ratio ratio-10x9 img-thumbnail" width={50} height={50} src="https://icones.pro/wp-content/uploads/2021/02/symbole-masculin-icone-l-utilisateur-vert.png" />
+                                     <h5 className="centrar mt-4">USUARIO 1</h5>
                 <div class="d-grid gap-2 col-6 mx-auto mt-4">
   <button class="btn btn-primary" type="button">Cerrar Sesión</button>
   
