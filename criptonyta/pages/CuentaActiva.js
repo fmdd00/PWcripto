ba839358e97c03cb6f0b244165cbc66e4d9f8866
Saori Arana <20180105@aloe.ulima.edu.pe>
2022-03-02 18:59:45 -0500

    .

--- criptonyta/pages/CuentaActiva.js
+++ criptonyta/pages/CuentaActiva.js
@@ -13,10 +13,10 @@ export default function CuentaActiva() {
                 <div className="col-6"><img src="https://img.freepik.com/vector-gratis/ganar-dinero-freelance-trabajo-remoto-trabajo-distancia-gente-negocios-hombre-laptop-sentada-billetera_203228-133.jpg?size=626&ext=jpg"/></div>
                 <div className="col-4">
                     <h3 className="mt-4">Actividad Reciente</h3>
-                    <p>-Transferencia de la cuenta en dólares a soles</p>
+                    <p>-Transferencia de la cuenta de Bitcoins a soles</p>
 
                     <h3>Operaciones Realizadas</h3>
-                    <p>- Se transfirió $50</p>
+                    <p>- Se transfirió S/50</p>
                     </div>
 
             </div>
