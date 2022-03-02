f3279c78cf3f94c98ddfdd9732254485ba6b2f4a
Saori Arana <20180105@aloe.ulima.edu.pe>
2022-03-02 18:45:46 -0500

    index

--- criptonyta/pages/index.js
+++ criptonyta/pages/index.js
@@ -5,7 +5,7 @@ import Login from "../components/Login.Components"
 import PiePagina from "../components/PiePagina.Components"
 export default function Home() {
   return <body className="fondoPrincipal">
-    <div  >
+    <div className="row"  >
       <Header />
       <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       <h1 className="titulo">Crypto-nita</h1>
@@ -14,14 +14,18 @@ export default function Home() {
           <Banner />
         </div>
         <div className="col mt-4 indextext">
-          <p>
+          
             Bienvenidos a nuestro proyecto Crypto-nita del curso de Programación
             Web. Crypto-nita es una página web que permite cambiar de manera 
             virtual los bitcoins a soles y/o soles a bitcoins.
-          </p>
-        </div>
+               
+           </div>
+           <div className="col-md-4 offset-md-7  "><img width={240} height={200} src="https://us.123rf.com/450wm/aihero/aihero1611/aihero161100016/66546110-le-revenu-passif-du-mobile-illustration-de-bande-dessin%C3%A9e-d-affaires.jpg"></img>
+             </div>
+           
       </div>
-      <div className="mt-4">
+      
+      <div className="mt-6">
         <Footer />
       </div>
     </div>
