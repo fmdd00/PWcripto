be3916b2c7013c80543c139c7284798bc26ecf05
Saori Arana <20180105@aloe.ulima.edu.pe>
2022-03-03 11:55:03 -0500

    registro

--- criptonyta/components/RegistroPaso2.component.js
+++ criptonyta/components/RegistroPaso2.component.js
@@ -1,6 +1,35 @@
 import { useRouter } from 'next/router'
+import { useState } from "react"
 
-export default function formatoRegistroPaso2() {
+export default function formatoRegistroPaso2(props) {
+
+    // Variables internas (variables de estado) del componente
+    const [txtCorreo, setTxtCorreo] = useState("")
+    const [txtPassword, setTxtPassword] = useState("")
+    const [txtRepPassword, setTxtRepPassword] = useState("")
+    const [txtTelefono, setTxtTelefono] = useState("")
+
+    const txtCorreoOnChange = (event) => {
+        const txtCorreoIngresado = event.target.value
+        setTxtCorreo(txtCorreoIngresado)
+        console.log(txtCorreoIngresado)
+    }
+
+    const txtPasswordOnChange = (event) => {
+        setTxtPassword(event.target.value)
+    }
+
+    const txtRepPasswordOnChange = (event) => {
+       setTxtRepPassword(event.target.value)
+   }
+   const txtTelefonoOnChange = (event) => {
+    setTxtTelefono(event.target.value)
+}
+
+    const butFinalizarOnClick = () => {
+        // Se va a ejecutar la funcion que se paso por el props (onSiguiente)
+        props.onFinalizar(txtCorreo, txtPassword,  txtRepPassword, txtTelefono)
+    }
 
     const router = useRouter()
 
@@ -10,28 +39,32 @@ export default function formatoRegistroPaso2() {
                 <h3 className="text-center">Paso 2 de 2</h3>
                 <form>
                     <div className="mt-4 input-group">
-                        <input id="txt_email" type="text" className="form-control" placeholder="Dirección de correo" />
+                        <input id="txt_correo" type="text" className="form-control" placeholder="Dirección de correo" 
+                        defaultValue={txtCorreo} onChange={txtCorreoOnChange}/>
                     </div>
                     <div className="mt-2">
-                        <input id="txt_password" type="password" className="form-control" placeholder="Contraseña" />
+                        <input id="txt_password" type="password" className="form-control" placeholder="Contraseña" 
+                        defaultValue={txtPassword} onChange={txtPasswordOnChange}/>
                     </div>
                     <div className="mt-2">
-                        <input id="txt_password" type="password" className="form-control" placeholder="Repite la contraseña" />
+                        <input id="txt_RepPassword" type="password" className="form-control" placeholder="Repite la contraseña" 
+                        defaultValue={txtRepPassword} onChange={txtRepPasswordOnChange}/>
                     </div>
                     <div className="mt-2 input-group">
-                        <span class="input-group-text" id="cod_pe">+ 51</span>
-                        <input id="txt_username" type="number" className="form-control" placeholder="Número telefónico" />
+                        <span className="input-group-text" id="cod_pe">+ 51</span>
+                        <input id="txt_telefono" type="number" className="form-control" placeholder="Número telefónico" 
+                        defaultValue={txtTelefono} onChange={txtTelefonoOnChange}/>
                     </div>
                     <div className="form-check mt-4">
                         <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
-                            <label className="form-check-label" for="flexCheckDefault">
+                            <label className="form-check-label" htmlFor="flexCheckDefault">
                                 <p> Acepto los <a className="link-primary" href="/TerminosCondiciones">Términos y Condiciones</a> de Crypto-Nita 
                                     y la <a className="link-primary" href="https://policies.google.com/terms">Política de privacidad</a> de Google y autorizo el tratamiento 
                                     de mis datos personales de conformidad con la Política de Privacidad.</p>
                             </label>
                     </div>
                     <div className="d-grid gap-2 mt-2">
-                        <a id="butLogin" className="btn btn-primary" type="button" href="/main">Finalizar</a>
+                        <a id="butLogin" className="btn btn-primary" type="button" href="/main" onClick={butFinalizarOnClick}>Finalizar</a>
                         <button id="butLogin" className="btn btn-secondary" type="button" onClick={ () => router.back() }>Regresar</button>
                     </div>
                 </form>
--- criptonyta/components/RegistroPrincipal.component.js
+++ criptonyta/components/RegistroPrincipal.component.js
@@ -1,7 +1,32 @@
 import { useRouter } from 'next/router'
+import { useState } from "react"
 
+export default function formatoRegistro(props) {
 
-export default function formatoRegistro() {
+     // Variables internas (variables de estado) del componente
+     const [txtNombre, setTxtNombre] = useState("")
+     const [txtApellido, setTxtApellido] = useState("")
+     const [txtDNI, setTxtDNI] = useState("")
+ 
+     const txtNombreOnChange = (event) => {
+         const txtNombreIngresado = event.target.value
+         setTxtNombre(txtNombreIngresado)
+         console.log(txtNombreIngresado)
+     }
+ 
+     const txtApellidoOnChange = (event) => {
+         setTxtApellido(event.target.value)
+         console.log(txtApellido)
+     }
+
+     const txtDNIOnChange = (event) => {
+        setTxtDNI(event.target.value)
+    }
+ 
+     const butSiguienteOnClick = () => {
+         // Se va a ejecutar la funcion que se paso por el props (onSiguiente)
+         props.onSiguiente(txtNombre, txtApellido,  txtDNI)
+     }
 
     const router = useRouter()
 
@@ -11,17 +36,21 @@ export default function formatoRegistro() {
                 <h3 className="text-center">Paso 1 de 2</h3>
                 <form>
                     <div className="mt-4 input-group">
-                        <input id="txt_email" type="text" className="form-control" placeholder="Nombres" />
+                        <input id="txt_nombre" type="text" className="form-control" placeholder="Nombres" 
+                        defaultValue={txtNombre} onChange={txtNombreOnChange}/>
                     </div>
                     <div className="mt-2 input-group">
-                        <input id="txt_username" type="text" className="form-control" placeholder="Apellidos" />
+                        <input id="txt_apellido" type="text" className="form-control" placeholder="Apellidos"
+                        defaultValue={txtApellido} onChange={txtApellidoOnChange} />
                     </div>
                     <div className="mt-2">
-                        <input id="txt_password" type="number" className="form-control" placeholder="Número de DNI" />
+                        <input id="txt_dni" type="number" className="form-control" placeholder="Número de DNI"
+                        defaultValue={txtDNI} onChange={txtDNIOnChange} />
                     </div>
                     
                     <div className="d-grid gap-2 mt-2">
-                        <a id="butLogin" className="btn btn-primary" type="button" href="/Registro2">Siguiente</a>
+                        <a id="butLogin" className="btn btn-primary" type="button" href="/Registro2"
+                        onClick={butSiguienteOnClick}>Siguiente</a>
                         <button id="butLogin" className="btn btn-secondary" type="button" onClick={ () => router.back() }>Regresar</button>
                     </div>
                 </form>
