9cca6005e45519b73e673959bf811690bf2d755b
Saori Arana <20180105@aloe.ulima.edu.pe>
2022-03-02 23:54:38 -0500

    TransacOp dao

--- criptonyta/dao/TransaccionOperacion.js
+++ criptonyta/dao/TransaccionOperacion.js
@@ -1,5 +1,44 @@
 const db = require("../sequelize/models")
 
-const
+const modificarEstado = (operacion) => {
+    const operacionStr = localStorage.getItem("operaciones")
+    if (operacionStr != null) {
+        const operaciones = JSON.parse(operacionStr)
+        for (let op of operaciones) {
+            if (operacion.id == op.id) {
+                op.estado = operacion.estado
+                break
+            }
+        }
+        localStorage.setItem("operaciones", JSON.stringify(operaciones))
+    }
+}
 
-export {}
+const guardarTransaccion=()=>{
+
+}
+
+
+const eliminarTransaccion=()=>{
+
+}
+
+
+const obtenerUsuariosOp = async () => {
+    const usuario = await db.TransaccionOperacion.findAll({
+        order : [
+            ["id", "ASC"]
+        ]
+    })
+    return usuario
+}
+
+const obtenerUsuarioOp = async (id) => {
+    return await db.TransaccionOperacion.findOne({
+        where : {
+            id : id
+        }
+    })
+}
+
+export {modificarEstado,obtenerUsuariosOp,obtenerUsuarioOp}
