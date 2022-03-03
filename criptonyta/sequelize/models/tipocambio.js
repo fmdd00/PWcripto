8ea26aa082edfdbe1463a3f059566f1e7279434f
Saori Arana <20180105@aloe.ulima.edu.pe>
2022-03-02 22:20:03 -0500

    db, falta fk

--- criptonyta/sequelize/models/tipocambio.js
+++ criptonyta/sequelize/models/tipocambio.js
@@ -11,6 +11,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      TipoCambio.hasMany(models.TransaccionOperacion,{
+        foreignKey:'idTipoCambio'
+      })
     }
   }
   TipoCambio.init({
