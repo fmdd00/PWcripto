6f1920f3c73aa8733f730012412c11d355bda7a9
fmdd00 <francisco63554@hotmail.com>
2022-03-03 00:19:30 -0500

    regresar a compra venta

--- criptonyta/pages/OperacionRealizadA.js
+++ criptonyta/pages/OperacionRealizadA.js
@@ -4,14 +4,17 @@ import CuerpoVenta from "../components/CuerpoVenta.Components";
 
 export default function EnviarVender() {
     return <div >
-        <VentaCabecera/>
+        <VentaCabecera />
         <h2 className="venta">Operacion Realizada</h2>
         <div className="card-body">
-        <h4 className="mt-5">Número de Operación:</h4>
-        <h1 className="mt-2">00000000111111222222222222</h1>
+            <h4 className="mt-5">Número de Operación:</h4>
+            <h1 className="mt-2">00000000111111222222222222</h1>
+        </div>
+        <div className="text-center">
+            <a className="btn btn-primary" href="/VerTipoCambio">Regresar a Compra / Venta</a>
         </div>
         <div>
-        <Footer4/>
+            <Footer4 />
         </div>
     </div>
 }
