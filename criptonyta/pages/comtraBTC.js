3482d744aa2bbdb053a5db72400c0574fd2a86b4
fmdd00 <francisco63554@hotmail.com>
2022-03-03 13:20:46 -0500

    comprabtc

--- criptonyta/pages/comtraBTC.js
+++ criptonyta/pages/comtraBTC.js
@@ -1,6 +1,21 @@
 import Footer2 from "../components/Footer2.Components"
 import Activa from "../components/Activa.Components"
+import { useState } from "react"
+
 export default function MainPage() {
+
+    const [monto, setMonto] = useState('')
+
+    const handleInputChange = ({ target }) =>{
+        setMonto(target.value)
+    }
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+
+        console.log(monto)
+    }
+
     return <div>
         <Activa />
         <h2><nav >
@@ -27,16 +42,18 @@ export default function MainPage() {
                 <div className="card">
                     <div className="card-body">
                         <h3 className="text-center">Calcular Cambio</h3>
-                        <form>
-                            <div className="mt-4">
-                                <input id="txt_username" type="text" className="form-control" placeholder="BTC" />
+                        <form onSubmit={ handleSubmit }>
+                            <div className="mt-4 input-group">
+                                <span className="input-group-text" >S/.</span>
+                                <input id="txt_password" type="text" className="form-control" placeholder="Ingrese cantidad en soles" value={ monto } onChange={ handleInputChange }/>
                             </div>
-                            <div className="mt-2">
-                                <input id="txt_password" type="password" className="form-control" placeholder="S/." />
+                            <div className="mt-2 input-group">
+                                <span className="input-group-text" >BTC</span>
+                                <input id="txt_username" type="text" className="form-control" placeholder="Usted obtendrá" />
                             </div>
                             <div></div>
                             <div className="d-grid gap-2 mt-2">
-                                <a className="btn btn-primary" type="button" href="#">Calcular</a>
+                                <button className="btn btn-primary" type="submit">Calcular</button>
                                 <a className="btn btn-secondary" type="button" href="/SolicitarCompra">Solicitar</a>
                             </div>
                         </form>
